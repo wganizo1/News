12115de99532595ab85255ab46595f239e012f30
Wellington <mobdev04@gmail.com>
2022-12-17 17:36:38 +0200

    My Constants - BaseUrl, API Key and top headline endpoint

---
+++ app/src/main/java/com/wganizo/news/contants/Constants.kt
@@ -0,0 +1,15 @@
+package com.wganizo.news.contants
+
+
+class Constants{
+
+    //API Key
+    val apiKey = "f21f9c0cd1fd4f1f8a75381872ef3765"
+
+    //Base Url
+    val baseUrl = "https://newsapi.org"
+
+    //ENDPOINTS
+    val topHeadlines = "/v2/top-headlines"
+
+}
